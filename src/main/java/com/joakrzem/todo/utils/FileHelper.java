0494220919388241a0744c871206e11a3945ec71
joakrzem <joannakrzeminska1997@wp.pl>
2020-10-16 16:59:51 +0200

    Added FileHelper to load files into Strings

---
+++ src/main/java/com/joakrzem/todo/utils/FileHelper.java
@@ -0,0 +1,33 @@
+package com.joakrzem.todo.utils;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FileHelper {
+    public String readFile(String name) {
+        InputStream is = FileHelper.class.getClassLoader().getResourceAsStream(name);
+
+        if (is != null) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+
+        return null;
+    }
+
+    public List<String> readFileLines(String name){
+        String file = readFile(name);
+        if (file == null){
+            return List.of();
+        }
+
+        return Arrays.stream(file.split("\n"))
+                .map(line -> line.replace("\n", "").replace("\r",""))
+                .collect(Collectors.toList());
+    }
+
+}
---
+++ src/test/java/com/joakrzem/todo/utils/FileHelperTest.java
@@ -0,0 +1,43 @@
+package com.joakrzem.todo.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileHelperTest {
+    // Jak podasz nazwe pliku co nie istnieje ma podac pusta liste
+    // Sprawdza czy jest podzielone na linijki
+    //czy lista ma dlugosc 4
+    //czy pod 0 jest linie 1 (wszystkie)
+    FileHelper fileHelper;
+
+    @BeforeEach
+    void setUp (){
+        fileHelper = new FileHelper();
+    }
+
+    @Test
+    void readFileLines_ShouldReturnEmptyList (){
+        //When
+        List<String> result = fileHelper.readFileLines("not-existing.txt");
+
+        //Then
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void readFileLines_ShouldDeviceStringToLines() {
+        //When
+        List<String> result = fileHelper.readFileLines("test.txt");
+
+        //Then
+        assertEquals(4, result.size());
+        assertEquals("Line1", result.get(0));
+        assertEquals("Line2", result.get(1));
+        assertEquals("Jack", result.get(2));
+        assertEquals("Steve", result.get(3));
+    }
+}
